3d16bc5b515e4b67240d78ae00b017b8a9224ffc
Pedro Costa <pedrodc23@gmail.com>
2017-10-29 16:56:53 +0000

    First commit

---
+++ FirstREST/App_Start/BundleConfig.cs
@@ -0,0 +1,81 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace FirstREST
+{
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                        "~/Scripts/jquery-ui-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.unobtrusive*",
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/dashboard").Include(
+                       "~/Content/vendors/jquery/dist/jquery.min.js",
+                       "~/Content/vendors/bootstrap/dist/js/bootstrap.min.js",
+                       "~/Content/vendors/fastclick/lib/fastclick.js",
+                       "~/Content/vendors/nprogress/nprogress.js",
+                       "~/Content/vendors/Chart.js/dist/Chart.min.js",
+                       "~/Content/vendors/gauge.js/dist/gauge.min.js",
+                       "~/Content/vendors/bootstrap-progressbar/bootstrap-progressbar.min.js",
+                       "~/Content/vendors/iCheck/icheck.min.js",
+                       "~/Content/vendors/skycons/skycons.js",
+                       "~/Content/vendors/Flot/jquery.flot.js",
+                       "~/Content/vendors/Flot/jquery.flot.pie.js",
+                       "~/Content/vendors/Flot/jquery.flot.time.js",
+                       "~/Content/vendors/Flot/jquery.flot.stack.js",
+                       "~/Content/vendors/Flot/jquery.flot.resize.js",
+                       "~/Content/vendors/flot.orderbars/js/jquery.flot.orderBars.js",
+                       "~/Content/vendors/flot-spline/js/jquery.flot.spline.min.js",
+                       "~/Content/vendors/flot.curvedlines/curvedLines.js",
+                       "~/Content/vendors/DateJS/build/date.js",
+                       "~/Content/vendors/jqvmap/dist/jquery.vmap.js",
+                       "~/Content/vendors/jqvmap/dist/maps/jquery.vmap.world.js",
+                       "~/Content/vendors/jqvmap/examples/js/jquery.vmap.sampledata.js",
+                       "~/Content/vendors/moment/min/moment.min.js",
+                       "~/Content/vendors/bootstrap-daterangepicker/daterangepicker.js",
+                       "~/Scripts/custom.js"
+                       ));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/Site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/dashboard").Include(
+                "~/Content/vendors/bootstrap/dist/css/bootstrap.css",
+                "~/Content/vendors/font-awesome/css/font-awesome.css",
+                "~/Content/vendors/nprogress/nprogress.css",
+                "~/Content/vendors/iCheck/skins/flat/green.css",
+                "~/Content/vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.css",
+                "~/Content/vendors/jqvmap/dist/jqvmap.css",
+                "~/Content/vendors/vendors/bootstrap-daterangepicker/daterangepicker.css"
+                ));
+
+            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+                        "~/Content/themes/base/jquery.ui.core.css",
+                        "~/Content/themes/base/jquery.ui.resizable.css",
+                        "~/Content/themes/base/jquery.ui.selectable.css",
+                        "~/Content/themes/base/jquery.ui.accordion.css",
+                        "~/Content/themes/base/jquery.ui.autocomplete.css",
+                        "~/Content/themes/base/jquery.ui.button.css",
+                        "~/Content/themes/base/jquery.ui.dialog.css",
+                        "~/Content/themes/base/jquery.ui.slider.css",
+                        "~/Content/themes/base/jquery.ui.tabs.css",
+                        "~/Content/themes/base/jquery.ui.datepicker.css",
+                        "~/Content/themes/base/jquery.ui.progressbar.css",
+                        "~/Content/themes/base/jquery-jvectormap-2.0.3.css",
+                        "~/Content/themes/base/jquery.ui.theme.css"));
+        }
+    }
+}
---
+++ FirstREST/Lib_Primavera/PriEngine.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Interop.ErpBS900;         // Use Primavera interop's [Path em C:\Program Files\Common Files\PRIMAVERA\SG800]
+using Interop.StdPlatBS900;
+using Interop.StdBE900;
+using ADODB;
+using Interop.IGcpBS900;
+
+namespace FirstREST.Lib_Primavera
+{
+    public class PriEngine
+    {
+
+        public static StdPlatBS Platform { get; set; }
+        public static ErpBS Engine { get; set; }
+
+        public static bool InitializeCompany(string Company, string User, string Password)
+        {
+
+            StdBSConfApl objAplConf = new StdBSConfApl();
+            StdPlatBS Plataforma = new StdPlatBS();
+            ErpBS MotorLE = new ErpBS();
+
+            EnumTipoPlataforma objTipoPlataforma = new EnumTipoPlataforma();
+            objTipoPlataforma = EnumTipoPlataforma.tpProfissional;
+
+            objAplConf.Instancia = "Default";
+            objAplConf.AbvtApl = "GCP";
+            objAplConf.PwdUtilizador = Password;
+            objAplConf.Utilizador = User;
+
+            StdBETransaccao objStdTransac = new StdBETransaccao();
+
+            // Opem platform.
+            Plataforma.AbrePlataformaEmpresa(ref Company, ref objStdTransac, ref objAplConf, ref objTipoPlataforma, ref Password);
+       
+            
+            // Is plt initialized?
+            if (Plataforma.Inicializada)
+            {
+
+                // Retuns the ptl.
+                Platform = Plataforma;
+
+                bool blnModoPrimario = true;
+
+                // Open Engine
+                MotorLE.AbreEmpresaTrabalho(EnumTipoPlataforma.tpProfissional, ref Company, ref User, ref Password, ref objStdTransac, "Default", ref blnModoPrimario);
+
+                // Returns the engine.
+                Engine = MotorLE;
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+        }
+
+    }
+
+}
