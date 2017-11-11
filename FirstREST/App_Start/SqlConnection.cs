bdf40c10f47a1c977d6fb93d4aef31972e3aacd5
Pedro Costa <pedrodc23@gmail.com>
2017-11-11 17:03:26 +0000

    Accessing company information on the home page

--- FirstREST/App_Start/SqlConnection.cs
+++ FirstREST/App_Start/SqlConnection.cs
@@ -1,30 +1,39 @@
 ﻿using System.Web;
 using System.Web.Mvc;
 
-public class SqlConnection
-{     
-    public static void load_db(){
 
-      string connectionString = GetConnectionString();  
-      
-      try{
+namespace FirstREST
+{
+    public class SqlConnection
+    {
+        public static System.Data.SqlClient.SqlConnection connection;
+        public static void load_db()
+        {
 
-          var connection = new System.Data.SqlClient.SqlConnection(connectionString);
+            string connectionString = GetConnectionString();
+            
+            try
+            {
+                connection = new System.Data.SqlClient.SqlConnection(connectionString);
 
-          using (connection){
-              connection.Open();
-          }
-          System.Diagnostics.Debug.WriteLine("Connected");
+                using (connection)
+                {
+                    connection.Open();
+                }
+                System.Diagnostics.Debug.WriteLine("Connected");
 
-      }
-      catch (System.Exception e){
-           System.Diagnostics.Debug.WriteLine("Failed to connect");
-           System.Diagnostics.Debug.WriteLine(e);
-      }
-	
-    }
+            }
+            catch (System.Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine("Failed to connect");
+                System.Diagnostics.Debug.WriteLine(e);
+            }
 
-    static private string GetConnectionString(){
-        return "Server=USER-PC\\PRIMAVERA;Database=staging;User Id=sa; Password=Feup2014";
+        }
+
+        static public string GetConnectionString()
+        {
+            return "Server=USER-PC\\PRIMAVERA;Database=staging;User Id=sa; Password=Feup2014";
+        }
     }
 }
