1ed0807d562c40f99dc48020c05697c213e1864d
Pedro Costa <pedrodc23@gmail.com>
2017-11-11 12:19:48 +0000

    Created controllers and views for each dashboard

---
+++ FirstREST/Controllers/InventoryController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstREST.Controllers
+{
+    public class InventoryController : Controller
+    {
+        //
+        // GET: /Inventory/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ FirstREST/Controllers/ManagementController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstREST.Controllers
+{
+    public class ManagementController : Controller
+    {
+        //
+        // GET: /Management/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
