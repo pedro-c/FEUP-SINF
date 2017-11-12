1751598319387624c15a45338d319020efba8659
Maria João Mira Paulo <mariajoaomirapaulo1996@gmail.com>
2017-11-12 18:01:03 +0000

    Continuing with dashboards

--- FirstREST/Controllers/InventoryController.cs
+++ FirstREST/Controllers/InventoryController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,9 +13,50 @@ namespace FirstREST.Controllers
         //
         // GET: /Inventory/
 
+        public class InventoryModel
+        {
+            public List<SupplierModel> suppliers = new List<SupplierModel>();
+        }
+
+        public class SupplierModel
+        {
+            public int id;
+            public string name;
+            public string phoneNumber;
+            public string email;
+        }
+
         public ActionResult Index()
         {
-            return View();
+            DataSet supplierTable = new DataSet();
+            InventoryModel InventoryDashboardModel = new InventoryModel();
+
+            string connectionString = FirstREST.SqlConnection.GetConnectionString();
+
+            using (System.Data.SqlClient.SqlConnection connection = new System.Data.SqlClient.SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand("Select * From dbo.Supplier", connection))
+                {
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                    {
+
+                        adapter.Fill(supplierTable, "Supplier");
+
+                        foreach (DataRow row in supplierTable.Tables["Supplier"].Rows)
+                        {
+                            SupplierModel temp_supplier = new SupplierModel();
+                            temp_supplier.id = row.Field<int>("id");
+                            temp_supplier.name = row.Field<string>("name");
+                            temp_supplier.phoneNumber = row.Field<string>("phoneNumber");
+                            temp_supplier.email = row.Field<string>("email");
+                            //temp_invoice.grossTotal = Convert.ToString(row.Field<float>("grossTotal"));
+                            InventoryDashboardModel.suppliers.Add(temp_supplier);
+                        }
+
+                        return View(InventoryDashboardModel);
+                    }
+                }
+            }
         }
     }
 }
