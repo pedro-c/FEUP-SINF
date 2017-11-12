ee6f53af2f77fbcc412f50361916c8020a2b97ad
Pedro Costa <pedrodc23@gmail.com>
2017-11-12 12:06:44 +0000

    Getting accounts from xml and adding to DB

--- FirstREST/App_Start/XmlLoader.cs
+++ FirstREST/App_Start/XmlLoader.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.SqlClient;
 using System.Web;
 using System.Web.Mvc;
 using System.Xml;
@@ -9,21 +10,35 @@ namespace FirstREST
 {
     public class XmlLoader
     {
+        public static XmlDocument saft = new XmlDocument();
+        public static string connectionString = FirstREST.SqlConnection.GetConnectionString();
         public static void readSaft()
         {
-            XDocument saft = XDocument.Load("C:\\SINF\\FEUP-SINF\\FirstREST\\SAFT.xml");
+            saft.Load("C:\\SINF\\FEUP-SINF\\FirstREST\\SAFT.xml");
 
-            foreach (XElement element in saft.Elements("Account"))
-            {
-                System.Diagnostics.Debug.WriteLine("Reading SAFT file");
-                System.Diagnostics.Debug.WriteLine(element.Attribute("AccountID").Value);
-            }
-
+            proccessAccounts(saft.GetElementsByTagName("Account"));
         }
 
-        public void proccessHeader(XElement header)
+        public static void proccessAccounts(XmlNodeList accounts)
         {
+            using (System.Data.SqlClient.SqlConnection connection = new System.Data.SqlClient.SqlConnection(connectionString))
+            {
+                connection.Open();
+                foreach (XmlNode account in accounts)
+                {
+                    var query = "INSERT INTO dbo.Account(id,accountDescription,openingDebitBalance,openingCreditBalance,closingDebitBalance,closingCreditBalance) VALUES(@id,@accountDescription,@openingDebitBalance,@openingCreditBalance,@closingDebitBalance,@closingCreditBalance)";
+                    using(var command = new SqlCommand(query, connection)){
+                        command.Parameters.AddWithValue("@id", account["AccountID"].InnerText);
+                        command.Parameters.AddWithValue("@accountDescription", account["AccountDescription"].InnerText);
+                        command.Parameters.AddWithValue("@openingDebitBalance", account["OpeningDebitBalance"].InnerText);
+                        command.Parameters.AddWithValue("@openingCreditBalance", account["OpeningCreditBalance"].InnerText);
+                        command.Parameters.AddWithValue("@closingDebitBalance", account["ClosingDebitBalance"].InnerText);
+                        command.Parameters.AddWithValue("@closingCreditBalance", account["ClosingCreditBalance"].InnerText);
+                        command.ExecuteNonQuery();
+                    }
 
+                }
+            }
         }
     }
 }
