f0166e13a431e71459d95588c4974f734f0cfdfb
Pedro Costa <pedrodc23@gmail.com>
2017-11-12 11:37:38 +0000

    Starting parser

---
+++ FirstREST/App_Start/XmlLoader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+using System.Xml.Linq;
+
+
+namespace FirstREST
+{
+    public class XmlLoader
+    {
+        public static void readSaft()
+        {
+            XDocument saft = XDocument.Load("C:\\SINF\\FEUP-SINF\\FirstREST\\SAFT.xml");
+
+            foreach (XElement element in saft.Elements("Account"))
+            {
+                System.Diagnostics.Debug.WriteLine("Reading SAFT file");
+                System.Diagnostics.Debug.WriteLine(element.Attribute("AccountID").Value);
+            }
+
+        }
+
+        public void proccessHeader(XElement header)
+        {
+
+        }
+    }
+}
---
+++ FirstREST/Controllers/SaftController.cs
@@ -0,0 +1,29 @@
+﻿using Interop.GcpBE900;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstREST.Controllers
+{
+    public class SaftController : Controller
+    {
+        //
+        // GET: /Saft/
+
+        public ActionResult Index()
+        {
+
+            if (Lib_Primavera.PriEngine.InitializeCompany("DEMOSINF","user","Feup2017."))
+            {
+                var saft = new GcpBESaftExportacao();
+                Lib_Primavera.PriEngine.Engine.Comercial.SaftExportacoes.GeraFicheiro(saft);
+
+                return View(saft);
+            }
+            return View();
+        }
+
+    }
+}
