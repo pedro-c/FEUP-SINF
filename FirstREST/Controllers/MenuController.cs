eb7e6fa20e0965220f9d980c2405d315503ef3a3
Pedro Costa <pedrodc23@gmail.com>
2017-12-12 09:26:36 +0000

    Separated header from layout

---
+++ FirstREST/Controllers/MenuController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstREST.Controllers
+{
+    public class MenuController : Controller
+    {
+        public class CompanyModel
+        {
+            public DateTime startDate { get; set; }
+            public DateTime endDate { get; set; }
+            public string companyName { get; set; }
+            public string fiscalYear { get; set; }
+            public string city { get; set; }
+            public string country { get; set; }
+            public string street { get; set; }
+        }
+
+       [ChildActionOnly]
+        public ActionResult Header()
+        {
+            DataTable company = new DataTable();
+            CompanyModel CompanyInformation = new CompanyModel();
+
+            string connectionString = FirstREST.SqlConnection.GetConnectionString();
+
+            using (System.Data.SqlClient.SqlConnection connection = new System.Data.SqlClient.SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand("Select * From dbo.Company", connection))
+                {
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                    {
+                        adapter.Fill(company);
+                        CompanyInformation.startDate = company.Rows[0].Field<DateTime>("StartDate");
+                        CompanyInformation.endDate = company.Rows[0].Field<DateTime>("EndDate");
+                        CompanyInformation.fiscalYear = company.Rows[0].Field<String>("FiscalYear");
+                        CompanyInformation.companyName = company.Rows[0].Field<String>("CompanyName");
+                        CompanyInformation.city = company.Rows[0].Field<String>("City");
+                        CompanyInformation.country = company.Rows[0].Field<String>("Country");
+                        CompanyInformation.street = company.Rows[0].Field<String>("StreetName");
+
+                        return PartialView("~/Views/Shared/_Header.cshtml", CompanyInformation);
+                    }
+                }
+            }
+        }
+
+    }
+}
